ef0191ac9cff8967242ff64bbca531b9872e7c69
qwer45us <agni.tripathi@kellogg.northwestern.edu>
2022-05-13 21:33:43 +0000

    End of class

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,21 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def show
+    @posts = Post.find_by({ "id" => params["id"] })
+  end
+
+  def new
+    @posts = Post.new
+  end
+
+  def create
+    @posts = Post.new
+    @posts["author"] = params["post"]["author"]
+    @posts["body"] = params["post"]["body"]
+    @posts["image"] = params["post"]["image"]
+    @posts.save
+    redirect_to "/posts"
+  end
+
 end
